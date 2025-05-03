e06ccb09937d30d73a858774f4e39028b8654826
one-with-violets-in-her-lap <walkways@proton.me>
2025-05-03 13:12:16 +0500

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
     return (
         <>
-            <h1>SQUARES</h1>
+            <h1 className="text-4xl">SQUARES</h1>
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import './global.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
