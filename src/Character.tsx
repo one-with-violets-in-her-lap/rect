3f1d1343a4826ee4468ecd9b95446363b919cfa8
one-with-violets-in-her-lap <walkways@proton.me>
2025-05-04 12:55:02 +0500

    implement basic horizontal movement

--- src/Character.tsx
+++ src/Character.tsx
@@ -1,15 +1,106 @@
 import characterImage from '@/assets/images/character-1.png'
 
-import { Assets, FederatedEvent, Sprite, Texture } from 'pixi.js'
+import { Assets, Sprite, Texture } from 'pixi.js'
 import { useEffect, useRef, useState } from 'react'
+import { useTick } from '@pixi/react'
+
+type JumpState =
+    | {
+          status: 'jumping' | 'landing'
+          initialYPosition: number
+      }
+    | {
+          status: 'inactive'
+          initialYPosition: null
+      }
+
+const MOVING_AMOUNT_VALUES = {
+    moveByPerTick: 4,
+    jumpAmountPerTick: 12,
+    maxJumpHeight: 20,
+    landingAmountPerTick: 10,
+}
+
+const SPRITE_SIZE = {
+    width: 112,
+    height: 116,
+}
 
 export function Character() {
     const sprite = useRef<Sprite>(null)
 
-    const isDragging = useRef(false)
-
     const [texture, setTexture] = useState(Texture.EMPTY)
+
     const [position, setPosition] = useState({ x: 0, y: 0 })
+
+    const [horizontalMovement, setHorizontalMovement] = useState<{
+        movingLeft: boolean
+        movingRight: boolean
+    }>({ movingLeft: false, movingRight: false })
+
+    const [jumpState, setJumpState] = useState<JumpState>({
+        status: 'inactive',
+        initialYPosition: null,
+    })
+
+    function handleGlobalKeyDown(event: KeyboardEvent) {
+        if (event.repeat) {
+            return
+        }
+
+        if (event.key === 'd') {
+            setHorizontalMovement((currentMovement) => ({
+                ...currentMovement,
+                movingRight: true,
+            }))
+        }
+
+        if (event.key === 'a') {
+            setHorizontalMovement((currentMovement) => ({
+                ...currentMovement,
+                movingLeft: true,
+            }))
+        }
+
+        if (event.key === ' ' && jumpState.status !== 'landing') {
+            setJumpState({
+                initialYPosition: position.y,
+                status: 'jumping',
+            })
+        }
+    }
+
+    function handleGlobalKeyUp(event: KeyboardEvent) {
+        if (event.key === 'd') {
+            setHorizontalMovement((currentMovement) => ({
+                ...currentMovement,
+                movingRight: false,
+            }))
+        }
+
+        if (event.key === 'a') {
+            setHorizontalMovement((currentMovement) => ({
+                ...currentMovement,
+                movingLeft: false,
+            }))
+        }
+    }
+
+    useTick(() => {
+        if (horizontalMovement.movingLeft) {
+            setPosition((currentPosition) => ({
+                x: currentPosition.x - MOVING_AMOUNT_VALUES.moveByPerTick,
+                y: currentPosition.y,
+            }))
+        }
+
+        if (horizontalMovement.movingRight) {
+            setPosition((currentPosition) => ({
+                x: currentPosition.x + MOVING_AMOUNT_VALUES.moveByPerTick,
+                y: currentPosition.y,
+            }))
+        }
+    })
 
     useEffect(() => {
         let alreadyUnmounted = false
@@ -20,44 +111,33 @@ export function Character() {
             }
         })
 
+        setPosition({ y: window.innerHeight - SPRITE_SIZE.height, x: 0 })
+
+        const abortController = new AbortController()
+        document.addEventListener('keypress', handleGlobalKeyDown, {
+            signal: abortController.signal,
+        })
+        document.addEventListener(
+            'keyup',
+            (event) => handleGlobalKeyUp(event),
+            { signal: abortController.signal },
+        )
+
         return () => {
             alreadyUnmounted = true
+            abortController.abort()
         }
     }, [])
-
-    function handleDragStart() {
-        isDragging.current = true
-    }
-
-    function handleDragEnd() {
-        isDragging.current = false
-    }
-
-    function handleDrag(event: FederatedEvent) {
-        if (!sprite.current) {
-            throw new Error(
-                '`sprite` ref is `null`. It must be bound to pixi sprite component to work',
-            )
-        }
-
-        if (isDragging.current && sprite.current) {
-            setPosition({
-                x: event.pageX - sprite.current.width / 2,
-                y: event.pageY - sprite.current.height / 2,
-            })
-        }
-    }
 
     return (
         <pixiSprite
             ref={sprite}
             texture={texture}
+            height={SPRITE_SIZE.height}
+            width={SPRITE_SIZE.width}
             x={position.x}
             y={position.y}
             eventMode="static"
-            onPointerDown={handleDragStart}
-            onPointerUp={handleDragEnd}
-            onGlobalPointerMove={handleDrag}
         ></pixiSprite>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from '@/App.tsx'
 import { extend } from '@pixi/react'
-import { Sprite } from 'pixi.js'
+import { Application, Sprite } from 'pixi.js'
 
-extend({ Sprite })
+extend({ Sprite, Application })
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
