23a3997606deaf6a85a5867efd55e6255d55360b
one-with-violets-in-her-lap <walkways@proton.me>
2025-05-07 06:55:01 +0500

    implement basic webrtc communication

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,4 @@
+import Peer from 'peerjs'
 import { Application } from 'pixi.js'
 import { GameEntity } from '@/game-entity'
 import { Obstacle } from '@/obstacle'
@@ -6,6 +7,45 @@ import { RemoteCharacter } from '@/entities/character/remote-character'
 
 export async function createGame(canvasElement: HTMLCanvasElement) {
     const game = new Game()
+
+    // Web RTC testing code
+
+    const peer = new Peer({
+        debug: 2,
+    })
+
+    peer.on('open', (id) => {
+        console.log('My peer ID is: ' + id)
+
+        const otherEndPeerId = prompt(
+            `Your peer ID is ${id}. Fill in the ID of peer you want to connect to`,
+        )
+
+        const sendConnection = peer.connect(otherEndPeerId || '')
+
+        peer.on('connection', (receiveConnection) => {
+            console.log(
+                `Connected to ${receiveConnection.peer}. Starting sending and receiving`,
+            )
+
+            receiveConnection.on('data', (data) => {
+                console.log(`New message from ${receiveConnection.peer}: ${data}`)
+            })
+
+            sendConnection.on('open', () => {
+                console.log(`Send connection opened`)
+
+                setInterval(() => {
+                    console.log(`Sending hi to ${sendConnection.peer}`)
+                    sendConnection.send(`Hi from ${peer.id}`)
+                }, 1000)
+            })
+            sendConnection.on('error', (e) => console.error(e))
+        })
+        peer.on('error', (e) => console.error(e))
+    })
+
+    // End of Web RTC testing code
 
     game.entities = [
         new CurrentControlledCharacter(game),
