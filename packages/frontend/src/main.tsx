558753af8d5b2d3687d02580f92d115bbeaabc64
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-05-18 19:47:40 +0500

    frontend: install tailwind

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,6 +1,8 @@
+import '@/global.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from '@/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
