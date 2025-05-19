078f89039c9d0acb37aa000b3d3f67f8d571b5d2
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-05-19 10:32:26 +0500

    add prettier tw classes sort plugin

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,17 @@
 function App() {
+    function handleStart() {}
+
     return (
         <>
-            <div className="max-w-4xl mx-auto py-46 px-6">
-                <h1 className="text-6xl font-semibold mb-5">Rect</h1>
+            <div className="mx-auto max-w-4xl px-6 py-46">
+                <h1 className="mb-5 text-6xl font-semibold">Rect</h1>
 
-                <p className="text-xl mb-5">1v1 shooter game</p>
+                <p className="mb-5 text-xl">1v1 shooter game</p>
 
-                <button className="transition-all duration-300 text-xl px-8 py-2 border-2 border-transparent bg-primary text-background rounded-lg w-2xs shadow-lg shadow-black/10 hover:cursor-pointer hover:bg-primary/80 hover:scale-105 active:scale-95 active:duration-100 active:shadow-transparent">
+                <button
+                    className="bg-primary text-background hover:bg-primary/80 w-2xs rounded-lg border-2 border-transparent px-8 py-2 text-xl shadow-lg shadow-black/10 transition-all duration-300 hover:scale-105 hover:cursor-pointer active:scale-95 active:shadow-transparent active:duration-100"
+                    onClick={handleStart}
+                >
                     Start
                 </button>
             </div>
