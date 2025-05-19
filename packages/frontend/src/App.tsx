3fdf67f72eeb238bb8c7fecd01cf33269f644440
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-05-19 10:26:00 +0500

    create basic home page

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
     return (
         <>
-            <h1>Rect</h1>
+            <div className="max-w-4xl mx-auto py-46 px-6">
+                <h1 className="text-6xl font-semibold mb-5">Rect</h1>
+
+                <p className="text-xl mb-5">1v1 shooter game</p>
+
+                <button className="transition-all duration-300 text-xl px-8 py-2 border-2 border-transparent bg-primary text-background rounded-lg w-2xs hover:cursor-pointer hover:bg-primary/80 hover:scale-105 active:scale-95 active:duration-100">
+                    Start
+                </button>
+            </div>
         </>
     )
 }
