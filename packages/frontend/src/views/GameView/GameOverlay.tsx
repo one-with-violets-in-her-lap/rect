9ac465712a45a82bb4aa9eff9e3acdf90ffcd6a2
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-05-22 15:47:22 +0500

    frontend/overlay: add speaking indicator

--- packages/frontend/src/views/GameView/GameOverlay.tsx
+++ packages/frontend/src/views/GameView/GameOverlay.tsx
@@ -1,4 +1,6 @@
 import type { MultiPlayerSession } from '@core/index'
+import { buildClassName } from '@frontend/utils/class-names'
+import { MicIcon } from 'lucide-react'
 import { useEffect, useState } from 'react'
 
 export function GameOverlay({
@@ -16,5 +18,17 @@ export function GameOverlay({
         }
     }, [])
 
-    return <div>{muted ? 'Muted' : 'Speaking'}</div>
+    return (
+        <div className="absolute top-0 left-0 flex h-full w-full items-start justify-end overflow-hidden p-5">
+            <div
+                className={buildClassName(
+                    'bg-primary text-background flex items-center gap-x-2 rounded-3xl px-5 py-1 text-lg transition-all duration-300 ease-out',
+                    muted && 'scale-0 opacity-0',
+                )}
+            >
+                Speaking
+                <MicIcon className="animate-pulse" />
+            </div>
+        </div>
+    )
 }
