93ff39b704a15e7a49fc2d11708668d68cc4d21b
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-11-27 19:45:35 +0100

    core/light: tweak opacity

--- packages/core/src/lib/entities/light.ts
+++ packages/core/src/lib/entities/light.ts
@@ -46,7 +46,7 @@ export class PointLight extends BaseGameEntity<Graphics> {
     async load() {
         const pixiObject = new Graphics()
 
-	pixiObject.alpha = 0.4
+	pixiObject.alpha = 0.3
 
 	const blurFilter = new BlurFilter({ strength: 30, antialias: true, blendMode: 'soft-light'})
 	pixiObject.filters = [blurFilter]
